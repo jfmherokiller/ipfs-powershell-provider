6b1798ddb1626ce5ba2db6f8450568b6c1751a24
noah the goodra <peterpan0413@live.com>
2017-08-04 14:15:15 -0500

    Add project files.

---
+++ ipfs-powershell-provider/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipfs_powershell_provider
+{
+    public class Class1
+    {
+    }
+}

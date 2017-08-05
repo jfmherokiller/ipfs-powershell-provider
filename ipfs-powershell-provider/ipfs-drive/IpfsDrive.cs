c3fdc839ea8b7b11666f52e4b5fa9cc89e3213b0
noah the goodra <peterpan0413@live.com>
2017-08-05 01:57:44 -0500

    moved code around

--- ipfs-powershell-provider/ipfs-drive/IpfsDrive.cs
+++ ipfs-powershell-provider/ipfs-drive/IpfsDrive.cs
@@ -15,4 +15,47 @@ namespace ipfs_powershell_provider
             return base.NewDriveDynamicParameters();
         }
     }
+    [CmdletProvider("PinnedObjects", ProviderCapabilities.None)]
+    class PinnedObjectsProvider : ContainerCmdletProvider
+    {
+        protected override void GetItem(string path)
+        {
+            var pinnedObject = communications.IpfsPinCommands.IpfsPinLS(path);
+            if (null != pinnedObject)
+            {
+                WriteItemObject(pinnedObject, path, false);
+            }
+        }
+        protected override bool ItemExists(string path)
+        {
+            return communications.IpfsPinCommands.IpfsPinLS(path).Keys.Count != 0;
+        }
+        protected override bool IsValidPath(string path)
+        {
+            return true;
+        }
+    }
+    [CmdletProvider("MfsDrive", ProviderCapabilities.None)]
+    class MfsDataProvider : ContainerCmdletProvider
+    {
+        protected override void GetItem(string path)
+        {
+            var mfsObject = communications.IpfsFilesCommands.ipfsFilesLs(path);
+            if (null != mfsObject)
+            {
+                if (mfsObject.Entries.Count > 1)
+                {
+                    WriteItemObject(mfsObject, path, true);
+                }
+            }
+        }
+        protected override bool ItemExists(string path)
+        {
+            return communications.IpfsFilesCommands.ipfsFilesLs(path).Entries.Count != 0;
+        }
+        protected override bool IsValidPath(string path)
+        {
+            return true;
+        }
+    }
 }

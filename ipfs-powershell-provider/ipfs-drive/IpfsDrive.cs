efa07ab7b87f58c405e16b7a858f2f176915aa7a
noah the goodra <peterpan0413@live.com>
2017-08-05 02:26:53 -0500

    test

--- ipfs-powershell-provider/ipfs-drive/IpfsDrive.cs
+++ ipfs-powershell-provider/ipfs-drive/IpfsDrive.cs
@@ -4,7 +4,7 @@ using System.Management.Automation.Provider;
 namespace ipfs_powershell_provider
 {
     [CmdletProvider("IpfsDrive", ProviderCapabilities.None)]
-    public class IpfsDrive : DriveCmdletProvider
+    public class IpfsDrive : ContainerCmdletProvider
     {
         protected override PSDriveInfo NewDrive(PSDriveInfo drive)
         {
@@ -14,10 +14,38 @@ namespace ipfs_powershell_provider
         {
             return base.NewDriveDynamicParameters();
         }
+        protected override void GetItem(string path)
+        {
+            if (path.StartsWith("IpfsDrive:/pins/"))
+            {
+                var pinnedObject = communications.IpfsPinCommands.IpfsPinLS(path);
+                if (null != pinnedObject)
+                {
+                    WriteItemObject(pinnedObject, path, false);
+                }
+            }
+
+        }
+        protected override bool ItemExists(string path)
+        {
+            return communications.IpfsPinCommands.IpfsPinLS(path).Keys.Count != 0;
+        }
+        protected override bool IsValidPath(string path)
+        {
+            return true;
+        }
     }
     [CmdletProvider("PinnedObjects", ProviderCapabilities.None)]
     class PinnedObjectsProvider : ContainerCmdletProvider
     {
+        protected override PSDriveInfo NewDrive(PSDriveInfo drive)
+        {
+            return base.NewDrive(drive);
+        }
+        protected override object NewDriveDynamicParameters()
+        {
+            return base.NewDriveDynamicParameters();
+        }
         protected override void GetItem(string path)
         {
             var pinnedObject = communications.IpfsPinCommands.IpfsPinLS(path);
@@ -38,6 +66,14 @@ namespace ipfs_powershell_provider
     [CmdletProvider("MfsDrive", ProviderCapabilities.None)]
     class MfsDataProvider : ContainerCmdletProvider
     {
+        protected override PSDriveInfo NewDrive(PSDriveInfo drive)
+        {
+            return base.NewDrive(drive);
+        }
+        protected override object NewDriveDynamicParameters()
+        {
+            return base.NewDriveDynamicParameters();
+        }
         protected override void GetItem(string path)
         {
             var mfsObject = communications.IpfsFilesCommands.ipfsFilesLs(path);

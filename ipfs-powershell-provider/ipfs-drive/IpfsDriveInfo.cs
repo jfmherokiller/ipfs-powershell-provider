4afbc3e47d7299acacb97892f76c95ffaccce936
noah the goodra <peterpan0413@live.com>
2017-08-04 20:15:08 -0500

    made build output to central bin

--- ipfs-powershell-provider/ipfs-drive/IpfsDriveInfo.cs
+++ ipfs-powershell-provider/ipfs-drive/IpfsDriveInfo.cs
@@ -1,11 +1,19 @@
 ﻿using System.Management.Automation;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ipfs_powershell_provider
 {
     public class IpfsDrive: PSDriveInfo
     {
+        public PinnedObjectsProvider mypins;
         public IpfsDrive(PSDriveInfo driveInfo) : base(driveInfo)
         {
+            mypins = new PinnedObjectsProvider();
         }
+        public PinnedObjectsProvider PinnedObjectsProvider => mypins;
+    }
+
+    public class PinnedObjectsProvider
+    {
     }
 }
